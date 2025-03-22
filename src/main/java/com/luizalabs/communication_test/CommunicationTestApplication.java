f30fe80b3983f675f465ddb517243281bdd8900b
EvanderLucena <evander.willian@gmail.com>
2025-03-22 09:24:22 -0300

    init project

---
+++ src/main/java/com/luizalabs/communication_test/CommunicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.luizalabs.communication_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizalabs/communication_test/CommunicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizalabs.communication_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
